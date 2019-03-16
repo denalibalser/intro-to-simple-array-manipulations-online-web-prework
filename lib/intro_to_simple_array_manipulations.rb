ec0af230cd4f5483093a05f356fb10d0b3a560ae
Denali Balser <denali.balser167@gmail.com>
2019-03-16 13:34:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -43,8 +43,7 @@ def using_flatten(array)
 end
 
 def using_delete(array, string)
-  instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-  no_offense_steven = instructors.delete("Steven")
+  array.delete(string)
 end
 
 def using_delete_at(array, integer)
